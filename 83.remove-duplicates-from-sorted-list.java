245f1e5e698bf58235e61120a63c8b5206e3929e
Sichen Jiang <scj1512@gmail.com>
2021-10-27 17:06:36 -0500

    83 updated

--- 83.remove-duplicates-from-sorted-list.java
+++ 83.remove-duplicates-from-sorted-list.java
@@ -17,6 +17,8 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+
+        /* 先用cur跳过所有重复，再跟pre连接。e.g.[1,2,2,2,2,3,3]
         ListNode cur = head, pre = head;
         while (cur != null && cur.next != null) {
             while (cur.next != null && cur.val == cur.next.val) {
@@ -25,6 +27,18 @@ class Solution {
             cur = cur.next;
             pre.next = cur;
             pre = cur;
+        }
+        return head;
+    }
+    */
+
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
         }
         return head;
     }
